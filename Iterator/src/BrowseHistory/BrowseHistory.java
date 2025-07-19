010eb6f5a4943ebf6e0f51a615921e1ebe9198c8
Jorge Basilio <J.l.Basilio55@gmail.com>
2025-07-19 17:00:30 -0400

    Iterator pattern using browser history - changed the way the program is implemented by changing it from array list to array.

--- Iterator/src/BrowseHistory/BrowseHistory.java
+++ Iterator/src/BrowseHistory/BrowseHistory.java
@@ -4,40 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BrowseHistory {
-    private List<String> urls = new ArrayList<>();
+    private String[] urls = new String[8];
+    private int count;
 
     public void push(String url){
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop(){
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
-
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator createIterator(){
-        return new ListIterator(this);
+        return new ArrayIterator(this);
     }
 
-    public class ListIterator implements Iterator{
+    public class ArrayIterator implements Iterator{
         private BrowseHistory history;
         private int index;
 
-        public ListIterator(BrowseHistory history){
+        public ArrayIterator(BrowseHistory history){
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return (index < history.urls.size());
+            return (index < history.count);
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
