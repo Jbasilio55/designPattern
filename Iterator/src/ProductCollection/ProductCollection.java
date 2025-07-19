7702caa93d502a1170b56350d8566a0248aea332
Jorge Basilio <J.l.Basilio55@gmail.com>
2025-07-19 17:06:46 -0400

    Exercises project files added.

---
+++ Iterator/src/ProductCollection/ProductCollection.java
@@ -0,0 +1,12 @@
+package ProductCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCollection {
+    private List<Product> products = new ArrayList<>();
+
+    public void add(Product product) {
+        products.add(product);
+    }
+}
