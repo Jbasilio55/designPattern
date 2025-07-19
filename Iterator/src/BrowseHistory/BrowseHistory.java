9d1f29c3d56f5e9bf6f3747d4537c61575f94460
Jorge Basilio <J.l.Basilio55@gmail.com>
2025-07-19 14:44:57 -0400

    Iterator pattern using browser history, and using an arraylist

--- Iterator/src/BrowseHistory/BrowseHistory.java
+++ Iterator/src/BrowseHistory/BrowseHistory.java
@@ -1,4 +1,24 @@
 package BrowseHistory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowseHistory {
+    private List<String> urls = new ArrayList<>();
+
+    public void push(String url){
+        urls.add(url);
+    }
+
+    public String pop(){
+        var lastIndex = urls.size() - 1;
+        var lastUrl = urls.get(lastIndex);
+        urls.remove(lastUrl);
+
+        return lastUrl;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
 }
